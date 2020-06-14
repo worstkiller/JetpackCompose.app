6b1408136cc23251a1c62e3d4dc996b230d09182
vinaygaba <vinaygaba@gmail.com>
2020-06-14 14:32:13 -0700

    Added more data

--- src/utils/Data.tsx
+++ src/utils/Data.tsx
@@ -1,26 +1,110 @@
 import JetpackComposeMetadata from "../models/JetpackComposeMetadata";
 
 const classicAndroidVsJetpackComposeMap = new Map([
-    [
-      "AppBar",
-      new JetpackComposeMetadata("TopAppBar", "http://www.google.com", ""),
-    ],
-    ["Button", new JetpackComposeMetadata("Button", "", "")],
-    ["CardView", new JetpackComposeMetadata("Card", "", "")],
-    ["ConstraintLayout", new JetpackComposeMetadata("Constraint Layout", "", "")],
-    ["EditText", new JetpackComposeMetadata("TextField", "", "")],
-    ["FrameLayout", new JetpackComposeMetadata("Stack", "", "")],
-    ["EditText", new JetpackComposeMetadata("TextField", "", "")],
-    ["LinearLayout", new JetpackComposeMetadata("Row/Column", "", "")],
-    ["ProgressBar", new JetpackComposeMetadata("ProgressIndicator", "", "")],
-    ["RecyclerView", new JetpackComposeMetadata("AdapterList", "", "")],
-    ["Slider", new JetpackComposeMetadata("Slider", "", "")],
-    [
-      "ScrollView",
-      new JetpackComposeMetadata("Vertical/Horizontal Scroller", "", ""),
-    ],
-    ["TextView", new JetpackComposeMetadata("Text", "", "")],
-    ["Themes", new JetpackComposeMetadata("MaterialTheme", "", "")],
-  ]);
+  [
+    "AppBar",
+    new JetpackComposeMetadata(
+      "TopAppBar",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/DrawerAppActivity.kt?q=TopAppBar#L182",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#topappbar"
+    ),
+  ],
+  [
+    "Button",
+    new JetpackComposeMetadata(
+      "Button",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/ButtonActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/Button"
+    ),
+  ],
+  [
+    "CardView",
+    new JetpackComposeMetadata(
+      "Card",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L114",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#card"
+    ),
+  ],
+  [
+    "ConstraintLayout",
+    new JetpackComposeMetadata(
+      "Constraint Layout",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/layout/ConstraintLayoutActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/layout/package-summary#constraintlayout"
+    ),
+  ],
+  [
+    "EditText",
+    new JetpackComposeMetadata(
+      "TextField",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/text/TextFieldActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#FilledTextField(kotlin.String,%20kotlin.Function1,%20,%20,%20androidx.ui.core.Modifier,%20androidx.ui.text.TextStyle,%20,%20)"
+    ),
+  ],
+  [
+    "FrameLayout",
+    new JetpackComposeMetadata(
+      "Stack",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/stack/StackActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/layout/package-summary#stack"
+    ),
+  ],
+  [
+    "LinearLayout",
+    new JetpackComposeMetadata(
+      "Row/Column",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/AlertDialogActivity.kt#L38",
+      "https://developer.android.com/reference/kotlin/androidx/ui/layout/package-summary#column"
+    ),
+  ],
+  [
+    "ProgressBar",
+    new JetpackComposeMetadata(
+      "ProgressIndicator",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L272",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#linearprogressindicator"
+    ),
+  ],
+  [
+    "RecyclerView",
+    new JetpackComposeMetadata(
+      "AdapterList",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/scrollers/VerticalScrollableActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/foundation/package-summary#adapterlist"
+    ),
+  ],
+  [
+    "Slider",
+    new JetpackComposeMetadata(
+      "Slider",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L394",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#slider"
+    ),
+  ],
+  [
+    "ScrollView",
+    new JetpackComposeMetadata(
+      "Vertical/Horizontal Scroller",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/scrollers/HorizontalScrollableActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/foundation/package-summary#horizontalscroller"
+    ),
+  ],
+  [
+    "TextView",
+    new JetpackComposeMetadata(
+      "Text",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/text/CustomTextActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/foundation/package-summary#text"
+    ),
+  ],
+  [
+    "Themes",
+    new JetpackComposeMetadata(
+      "MaterialTheme",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/theme/DarkModeActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#materialtheme"
+    ),
+  ],
+]);
 
 export default classicAndroidVsJetpackComposeMap;
