9ed9f023ebba058c7a79da15b1c8dce8dc954b0c
vinaygaba <vinaygaba@gmail.com>
2020-06-14 17:51:18 -0700

    Added more data

--- src/utils/Data.tsx
+++ src/utils/Data.tsx
@@ -2,11 +2,43 @@ import JetpackComposeMetadata from "../models/JetpackComposeMetadata";
 
 const classicAndroidVsJetpackComposeMap = new Map([
   [
+    "AlertDialog",
+    new JetpackComposeMetadata(
+      "AlertDialog",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/AlertDialogActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#alertdialog"
+    ),
+  ],
+  [
     "AppBar",
     new JetpackComposeMetadata(
       "TopAppBar",
       "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/DrawerAppActivity.kt?q=TopAppBar#L182",
       "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#topappbar"
+    ),
+  ],
+  [
+    "android:background",
+    new JetpackComposeMetadata(
+      "Surface",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/text/CustomTextActivity.kt#L187",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#Surface(androidx.ui.core.Modifier,%20androidx.ui.graphics.Shape,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.foundation.Border,%20androidx.ui.unit.Dp,%20,%20)"
+    ),
+  ],
+  [
+    "android:layout_weight",
+    new JetpackComposeMetadata(
+      "Modifier.weight",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/layout/ViewLayoutConfigurationsActivity.kt#L78",
+      "https://developer.android.com/reference/kotlin/androidx/ui/layout/ColumnScope#weight"
+    ),
+  ],
+  [
+    "BottomAppBar",
+    new JetpackComposeMetadata(
+      "BottomAppBar",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/BottomNavigationActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#bottomappbar"
     ),
   ],
   [
@@ -26,11 +58,35 @@ const classicAndroidVsJetpackComposeMap = new Map([
     ),
   ],
   [
+    "Checkbox",
+    new JetpackComposeMetadata(
+      "Checkbox",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L157",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#checkbox"
+    ),
+  ],
+  [
     "ConstraintLayout",
     new JetpackComposeMetadata(
       "Constraint Layout",
       "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/layout/ConstraintLayoutActivity.kt",
       "https://developer.android.com/reference/kotlin/androidx/ui/layout/package-summary#constraintlayout"
+    ),
+  ],
+  [
+    "Dark Mode",
+    new JetpackComposeMetadata(
+      "Constraint Layout",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/theme/DarkModeActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#darkColorPalette(androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color,%20androidx.ui.graphics.Color)"
+    ),
+  ],
+  [
+    "DrawerLayout",
+    new JetpackComposeMetadata(
+      "ModalDrawerLayout",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/DrawerAppActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#modaldrawerlayout"
     ),
   ],
   [
@@ -42,11 +98,51 @@ const classicAndroidVsJetpackComposeMap = new Map([
     ),
   ],
   [
+    "FloatingActionButton",
+    new JetpackComposeMetadata(
+      "FloatingActionButton",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/FixedActionButtonActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#floatingactionbutton"
+    ),
+  ],
+  [
+    "flow.collect",
+    new JetpackComposeMetadata(
+      "flow.collectAsState",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/state/coroutine/CoroutineFlowActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/compose/package-summary#collectasstate"
+    ),
+  ],
+  [
     "FrameLayout",
     new JetpackComposeMetadata(
       "Stack",
       "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/stack/StackActivity.kt",
       "https://developer.android.com/reference/kotlin/androidx/ui/layout/package-summary#stack"
+    ),
+  ],
+  [
+    "getDrawable",
+    new JetpackComposeMetadata(
+      "loadImageResource",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/image/ImageActivity.kt#L110",
+      "https://developer.android.com/reference/kotlin/androidx/ui/res/package-summary#loadimageresource"
+    ),
+  ],
+  [
+    "ImageView",
+    new JetpackComposeMetadata(
+      "Image",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/image/ImageActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/foundation/package-summary#image"
+    ),
+  ],
+  [
+    "layer-list shadow",
+    new JetpackComposeMetadata(
+      "Modifier.drawShadow",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/ShadowActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/core/package-summary#drawshadow"
     ),
   ],
   [
@@ -58,11 +154,43 @@ const classicAndroidVsJetpackComposeMap = new Map([
     ),
   ],
   [
+    "liveData.observe",
+    new JetpackComposeMetadata(
+      "liveData.observeAsState",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/state/livedata/LiveDataActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/livedata/package-summary#(androidx.lifecycle.LiveData).observeAsState()"
+    ),
+  ],
+  [
+    "OnClickListener",
+    new JetpackComposeMetadata(
+      "Clickable",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/AlertDialogActivity.kt#L51",
+      "https://developer.android.com/reference/kotlin/androidx/ui/foundation/package-summary#clickable"
+    ),
+  ],
+  [
+    "Padding",
+    new JetpackComposeMetadata(
+      "Modifier.padding",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/layout/LayoutModifierActivity.kt#L51",
+      "https://developer.android.com/reference/kotlin/androidx/ui/core/Modifier"
+    ),
+  ],
+  [
     "ProgressBar",
     new JetpackComposeMetadata(
       "ProgressIndicator",
       "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L272",
       "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#linearprogressindicator"
+    ),
+  ],
+  [
+    "RadioGroup",
+    new JetpackComposeMetadata(
+      "RadioGroup",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L239",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#RadioGroup(kotlin.collections.List,%20kotlin.String,%20kotlin.Function1,%20androidx.ui.core.Modifier,%20androidx.ui.graphics.Color,%20androidx.ui.text.TextStyle)"
     ),
   ],
   [
@@ -74,11 +202,11 @@ const classicAndroidVsJetpackComposeMap = new Map([
     ),
   ],
   [
-    "Slider",
+    "SavedStateHandle",
     new JetpackComposeMetadata(
-      "Slider",
-      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L394",
-      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#slider"
+      "savedInstanceState",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/state/ProcessDeathActivity.kt",
+      "https://developer.android.com/reference/kotlin/androidx/ui/savedinstancestate/package-summary#savedinstancestate"
     ),
   ],
   [
@@ -87,6 +215,22 @@ const classicAndroidVsJetpackComposeMap = new Map([
       "Vertical/Horizontal Scroller",
       "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/scrollers/HorizontalScrollableActivity.kt",
       "https://developer.android.com/reference/kotlin/androidx/ui/foundation/package-summary#horizontalscroller"
+    ),
+  ],
+  [
+    "Slider",
+    new JetpackComposeMetadata(
+      "Slider",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L394",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#slider"
+    ),
+  ],
+  [
+    "Snackbar",
+    new JetpackComposeMetadata(
+      "Snackbar",
+      "https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt#L367",
+      "https://developer.android.com/reference/kotlin/androidx/ui/material/package-summary#Snackbar(,%20,%20,%20)"
     ),
   ],
   [
