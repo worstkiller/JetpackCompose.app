5fa30787a2c230f06889cea6904b2e2ab8c05f48
vinaygaba <vinaygaba@gmail.com>
2020-07-22 22:29:59 -0700

    Added more questions and reordered

--- src/utils/Data.tsx
+++ src/utils/Data.tsx
@@ -267,16 +267,46 @@ export const mockQnA: Array<QnA>  = [
     "An alpha release for Compose is expected in Summer of 2020 as per the last official announcement. I am expecting it to be in August but don't quote me on it 😜"
   ),
   new QnA(
-    "How will I use view models and live data with Compose?", 
-    "You can continue to use View Models and Live Data with Compose if it fits your use case. Here is an <a href='https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/state/livedata/LiveDataActivity.kt'>example</a> to help you."
+    "Will Fragments be deprecated once Compose is released?", 
+    "<p>Compose didn't start with the goal replacing fragments per se but it’s a fantastic side effect. In most use cases, you shouldn’t need fragments at all if you are starting a pure compose screen/app. Moreover, since there are millions of apps that still use Fragments, there will most likely be ways to use it with that, although definitely not a requirement. </p>"
+  ),
+  new QnA(
+    "Why was a complete rewrite of the UI toolkit necessary?", 
+    "<p>Compose was built  to solve some pain points that the Android developers had been facing since many years. Let's look at what some of those were:</p>" +
+
+    "<p><b>UI is tied to the Operating System</b><br/>The first reason is that the current UI toolkit is tied to the Android Operating System. What this means is that if we the Android team made some new improvements in the View system or as an example the View.java file, we will have to wait for an Android API release to get these improvements. It's not a simple library that I can just bump up to get these latest improvements & changes. Unbundling the view system from the OS would make it easier to quickly fix bugs and bring improvements in a backward compatible way.</p>" +
+    
+    "<p><b>State Management is tricky</b><br/>The way we managed UI state has always been this complicated dance that we have to do as views store own their own state. And we need to make sure that the state that we store in our view models and presenters is in sync with the state inside of the views. For example, if my business logic requires me to hide a view, that logic is stored in some view model or presenter and then as a reaction to that requirement, I need to get hold of the view I need to hide and explicitly change it's visibility by calling the setVisibility method on the view, which in turn changes the internal state of the view. Now this is really error prone and majority of the bugs in our apps stem from state management. So there has to be a better way.  </p>" +
+    
+    "<p><b>Lots of Context Switching</b><br/>Let's talk about the average workflow of an Android developer. We typically create views in xml, then create our screens which have lifecycles in them using Kotlin/Java. We then reference the views that we originally created in xml using id's/tags and then update its state. When we need to reference styles & dimensions, we again store them in xml. This constant context switching definitely has some impact or our productivity and we've come to accept this unfortunately. Now some folks might say that isn't it good that all the UI is in xml files and all business logic in Kotlin files. Decoupling is supposed to be good after all. Well, it's true that decoupling is a good engineering practice but this is not really decoupling because you are still referencing these xml files in your Kotlin code and making modification to it. So even though you might think that xml is decoupled and your UI code is decoupled, in reality it's just an illusion. </p>" +
+    
+    "<p><b>Simple things still require a lot of code</b><br/>We are used to writing a lot of code for even doing simple things. We just spoke about the normal workflow where we are making changes across so many files. Even outside of that, we have so many moving pieces that we need to take care of for writing UI code. Want to make a simple list, you create an adapter first, make sure that it extends the right class, override the relevant methods, If your list supports types of views items, then you override an additional method to pass the relevant layout xml file. So there's a lot of boilerplate code that we have to write for even doing simple things. The most effective way to reduce the number of bugs is to reduce the amount of code that we write. </p>"
+  ),
+  new QnA(
+    "Where can I see examples to learn about Jetpack Compose?", 
+    "If you haven't already noticed, here is another shameless 🔌- <a href='https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example'>Learn Jetpack Compose By Example</a>"
   ),
   new QnA(
     "How do I learn about the equivalent API for common Android tasks in Jetpack Compose?", 
     "I maintain a tool that helps you find the equivalent API's in Compose. You can use it <a href='https://jetpackcompose.app/'>here</a>"
   ),
   new QnA(
-    "Will Fragments be deprecated once Compose is released?", 
-    "<p>Compose didn't start with the goal replacing fragments per se but it’s a fantastic side effect. In most use cases, you shouldn’t need fragments at all if you are starting a pure compose screen/app. Moreover, since there are millions of apps that still use Fragments, there will most likely be ways to use it with that, although definitely not a requirement. </p>"
+    "How can I get started with Compose?", 
+    "You can see the <a href='https://developer.android.com/jetpack/compose/setup'>setup instructions here</a> to get your machine ready for doing Compose development. "
+  ),
+  new QnA(
+    "How do I get my apps ready for Compose?", 
+    "<p>If you apps are using uni-directional data flow, they will stand to benefit as the migration to Compose would be much smoother. I had spoken about a very relevant topic at Droidcon SF 2019. Towards the second half of the talk, I replace my entire UI with Compose with very little effort.</p>" +
+
+    "<p><a href='[https://www.droidcon.com/media-detail?video=380844265](https://www.droidcon.com/media-detail?video=380844265)'>Video</a>  |  <a href='[https://speakerdeck.com/vinaygaba/architecture-agnostic-ui-development](https://speakerdeck.com/vinaygaba/architecture-agnostic-ui-development)'>Slides</a></p>"
+  ),
+  new QnA(
+    "How would you describe a Composable?", 
+    "@Composable is the secret sauce for Jetpack Compose and it is the most fundamental building block. Annotating a function with @Composable allows that function to describe UI in Compose. This annotation is needed because Compose uses a custom kotlin compiler to function. This custom compiler does some post-processing to each @Composable function and changes its definition at compile time"
+  ),
+  new QnA(
+    "How will I use view models and live data with Compose?", 
+    "You can continue to use View Models and Live Data with Compose if it fits your use case. Here is an <a href='https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/state/livedata/LiveDataActivity.kt'>example</a> to help you."
   ),
   new QnA(
     "Does Compose work with WearOS?", 
@@ -303,56 +333,36 @@ export const mockQnA: Array<QnA>  = [
     "Testing Compose screens feels a lot like unit testing due to the super simple syntax that it provides. <a href='https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/androidTest/java/com/example/jetpackcompose/text/SimpleTextComposableTest.kt'>Check out this simple example.</a>"
   ),
   new QnA(
-    "Where can I see examples to learn about Jetpack Compose?", 
-    "If you haven't already noticed, here is another shameless 🔌- <a href='https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example'>Learn Jetpack Compose By Example</a>"
-  ),
-  new QnA(
     "Are material design widgets already available for use?", 
     "<p>A lot of widgets that adhere to the material design specification are already available in Compose. <a href='https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/blob/master/app/src/main/java/com/example/jetpackcompose/material/MaterialActivity.kt'>Here are some implementations of the Material Design widgets.</a></p>" +
     "<p><img src='https://github.com/vinaygaba/Learn-Jetpack-Compose-By-Example/raw/master/screenshots/material_design_components.gif' /></p>"
   ),
-  new QnA(
-    "How can I get started with Compose?", 
-    "TODO"
-  ),
-  new QnA(
-    "How does navigation work in Compose?", 
-    "TODO"
-  ),
-  new QnA(
-    "All examples show a lot of nested composables? Isn't nesting views a bad thing?", 
-    "TODO"
-  ),
-  new QnA(
-    "Why was a complete rewrite of the UI toolkit necessary?", 
-    "TODO"
-  ),
-  new QnA(
-    "How would styles and dimensions work in Compose?", 
-    "TODO"
-  ),
-  new QnA(
-    "How do I get my apps ready before Compose launches?", 
-    "TODO"
-  ),
-  new QnA(
-    "How would you describe a Composable?", 
-    "TODO"
-  ),
-  new QnA(
-    "How would you describe the lifecycle of a Composable?", 
-    "TODO"
-  ),
-  new QnA(
-    "How does compose handle process death?", 
-    "TODO"
-  ),
-  new QnA(
-    "How do animations work in Compose?", 
-    "TODO"
-  ),
-  new QnA(
-    "What kind of tooling is available with Compose?", 
-    "TODO"
-  ),
+  // new QnA(
+  //   "How does navigation work in Compose?", 
+  //   "TODO"
+  // ),
+  // new QnA(
+  //   "All examples show a lot of nested composables? Isn't nesting views a bad thing?", 
+  //   "TODO"
+  // ),
+  // new QnA(
+  //   "How would styles and dimensions work in Compose?", 
+  //   "TODO"
+  // ),
+  // new QnA(
+  //   "How would you describe the lifecycle of a Composable?", 
+  //   "TODO"
+  // ),
+  // new QnA(
+  //   "How does compose handle process death?", 
+  //   "TODO"
+  // ),
+  // new QnA(
+  //   "How do animations work in Compose?", 
+  //   "TODO"
+  // ),
+  // new QnA(
+  //   "What kind of tooling is available with Compose?", 
+  //   "TODO"
+  // ),
 ];
