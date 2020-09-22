c2fe5e976991aac602a2cb6653a72d95a4518885
vinaygaba <vinaygaba@gmail.com>
2020-09-21 23:37:48 -0700

    Added header to quickbites

--- src/components/QuickBitesComponent.tsx
+++ src/components/QuickBitesComponent.tsx
@@ -9,7 +9,18 @@ export default function QuickBitesComponent() {
   return (
     <Grid container className={classes.root} spacing={2}>
       <Grid item xs={12} md={12}>
-        <Grid container justify="center" spacing={2} xl>
+        <Grid container justify="center" spacing={2}>
+          <Grid key={"header"} item xs={12} md={4}>
+            <Typography className={classes.pageHeader} align="center">
+              Compose Quick Bites
+            </Typography>
+            <Typography className={classes.pageSubheader} align="center">
+              Learn more about the core concepts of Jetpack Compose right from
+              your Twitter feed!
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container justify="center" spacing={2}>
           <Grid key={"issue1"} item xs={12} md={4}>
             <Typography className={classes.issueTitle} align="center">
               Issue #1: What is declarative UI?
@@ -33,9 +44,24 @@ const useStyles = makeStyles({
     marginTop: "1%",
     padding: "4%",
   },
+  pageHeader: {
+    fontSize: 30,
+    fontFamily: "Playfair Display",
+    marginBottom: "1%",
+    color: "#78C257",
+  },
+  pageSubheader: {
+    fontSize: 20,
+    fontWeight: "lighter",
+    fontFamily: "Roboto",
+  },
   issueTitle: {
     fontSize: 20,
     fontFamily: "Playfair Display",
     marginBottom: "2%",
+    marginTop: "5%",
+    color: "#FFFFFF",
+    background: "#81c1eb",
+    padding: "5px",
   },
 });
