190d7d36e13160be1b6488adbae0f3d2aeec7a45
vinaygaba <vinaygaba@gmail.com>
2020-09-22 12:02:25 -0700

    Added footer

--- src/components/QuickBitesComponent.tsx
+++ src/components/QuickBitesComponent.tsx
@@ -1,5 +1,6 @@
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import JetpackComposeAppFooter from "./JetpackComposeAppFooter";
 import React from "react";
 import TweetEmbed from "react-tweet-embed";
 import Typography from "@material-ui/core/Typography";
@@ -7,42 +8,46 @@ import Typography from "@material-ui/core/Typography";
 export default function QuickBitesComponent() {
   const classes = useStyles();
   return (
-    <Grid container className={classes.root} spacing={2}>
-      <Grid item xs={12} md={12}>
-        <Grid container justify="center" spacing={2}>
-          <Grid key={"header"} item xs={12} md={4}>
-            <Typography className={classes.pageHeader} align="center">
-              Compose Quick Bites
-            </Typography>
-            <Typography className={classes.pageSubheader} align="center">
-              Learn more about the core concepts of Jetpack Compose right from
-              your Twitter feed!
-            </Typography>
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={12}>
+          <Grid container justify="center" spacing={2}>
+            <Grid key={"header"} item xs={12} md={4}>
+              <Typography className={classes.pageHeader} align="center">
+                Compose Quick Bites
+              </Typography>
+              <Typography className={classes.pageSubheader} align="center">
+                Learn more about the core concepts of Jetpack Compose right from
+                your Twitter feed!
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container justify="center" spacing={2}>
-          <Grid key={"issue1"} item xs={12} md={4}>
-            <Typography className={classes.issueTitle} align="center">
-              Issue #1: What is declarative UI?
-            </Typography>
-            <TweetEmbed id="1304904120868823040" />
-          </Grid>
-          <Grid key={"issue2"} item xs={12} md={4}>
-            <Typography className={classes.issueTitle} align="center">
-              Issue #2: What is this @Composable thing?
-            </Typography>
-            <TweetEmbed id="1307528586174160896" />
+          <Grid container justify="center" spacing={2}>
+            <Grid key={"issue1"} item xs={12} md={4}>
+              <Typography className={classes.issueTitle} align="center">
+                Issue #1: What is declarative UI?
+              </Typography>
+              <TweetEmbed id="1304904120868823040" />
+            </Grid>
+            <Grid key={"issue2"} item xs={12} md={4}>
+              <Typography className={classes.issueTitle} align="center">
+                Issue #2: What is this @Composable thing?
+              </Typography>
+              <TweetEmbed id="1307528586174160896" />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+      <div className={classes.footer}>
+        <JetpackComposeAppFooter />
+      </div>
+    </div>
   );
 }
 
 const useStyles = makeStyles({
   root: {
-    marginTop: "1%",
-    padding: "4%",
+    marginTop: "5%",
   },
   pageHeader: {
     fontSize: 30,
@@ -63,5 +68,16 @@ const useStyles = makeStyles({
     color: "#FFFFFF",
     background: "#81c1eb",
     padding: "5px",
+  },
+  footer: {
+    marginTop: "2%",
+    height: "20vh",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "#000000",
+    marginBottom: "0px",
+    bottom: "0px",
   },
 });
